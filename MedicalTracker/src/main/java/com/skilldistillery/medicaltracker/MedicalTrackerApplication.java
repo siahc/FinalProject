f3c8cf47493af1f20bb73fe6fc2628465ec26da9
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-12-04 14:37:39 -0600

    Create Spring REST stub

---
+++ MedicalTracker/src/main/java/com/skilldistillery/medicaltracker/MedicalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.medicaltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalTrackerApplication.class, args);
+	}
+
+}
---
+++ MedicalTracker/src/test/java/com/skilldistillery/medicaltracker/MedicalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.medicaltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
