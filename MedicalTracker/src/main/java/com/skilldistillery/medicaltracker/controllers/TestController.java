7575ff8728433b4b1484408bc86c039588deda41
Josiah Candler <69128122+siahc@users.noreply.github.com>
2020-12-04 15:41:29 -0700

    Configure Test

--- MedicalTracker/src/main/java/com/skilldistillery/medicaltracker/controllers/TestController.java
+++ MedicalTracker/src/main/java/com/skilldistillery/medicaltracker/controllers/TestController.java
@@ -1,9 +1,11 @@
 package com.skilldistillery.medicaltracker.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin({ "*", "http://localhost:4210" })
 @RestController
 @RequestMapping("api")
 public class TestController {
