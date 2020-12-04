25c7967d28bde623dcb636dcaaf9b95a9d56639b
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-12-04 14:49:47 -0600

    Create test endpoint

---
+++ MedicalTracker/src/main/java/com/skilldistillery/medicaltracker/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.medicaltracker.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class TestController {
+	
+	@GetMapping("ping")
+	public String ping() {
+		return "Pong";
+	}
+}
