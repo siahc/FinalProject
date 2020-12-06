be805c8b2328419ba1041370c1778c201f846af8
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-12-06 11:09:11 -0600

    Security/Auth began. Register and Auth functional

---
+++ MedicalTracker/src/main/java/com/skilldistillery/medicaltracker/controllers/AuthController.java
@@ -0,0 +1,36 @@
+package com.skilldistillery.medicaltracker.controllers;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.medicaltracker.entities.User;
+import com.skilldistillery.medicaltracker.services.AuthService;
+
+@RestController
+public class AuthController {
+	@Autowired
+	private AuthService authService;
+	
+	@PostMapping(path = "/register")
+	public User register(@RequestBody User user, HttpServletResponse res) {
+	    if (user == null) {
+	        res.setStatus(400);
+	    }
+
+	    user = authService.register(user);
+
+	    return user;
+	}
+
+	@GetMapping(path = "/authenticate")
+	public User authenticate(Principal principal) {
+	    return authService.getUser(principal.getName());
+	}
+}
--- MedicalTracker/src/main/java/com/skilldistillery/medicaltracker/repositories/UserRepository.java
+++ MedicalTracker/src/main/java/com/skilldistillery/medicaltracker/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.skilldistillery.medicaltracker.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+	User findUniqueByUsername(String username);
 }
