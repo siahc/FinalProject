fdd0592f4532df770ab912182982017b1437c6e7
Josiah Candler <69128122+siahc@users.noreply.github.com>
2020-12-05 18:53:26 -0700

    Add Provider Service/Impl/Controller

---
+++ MedicalTracker/src/main/java/com/skilldistillery/medicaltracker/controllers/ProviderController.java
@@ -0,0 +1,68 @@
+package com.skilldistillery.medicaltracker.controllers;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.medicaltracker.entities.Provider;
+import com.skilldistillery.medicaltracker.services.ProviderService;
+
+@RequestMapping("api")
+@RestController
+public class ProviderController {
+	
+	@Autowired
+	private ProviderService providerSvc;
+	
+	@GetMapping("provider")
+	public List<Provider> getProviders(){
+		return providerSvc.getAllProviders();
+	}
+	
+	@GetMapping("provider/{id}")
+	public Provider findProvider(
+		@PathVariable int id,
+		HttpServletResponse response
+		) {
+		if(id < 1) {
+			response.setStatus(400);
+			return null;
+		}
+		
+		Provider p = providerSvc.findById(id);
+		if (p == null) {
+			response.setStatus(404);
+		}
+		return p;
+	}
+	
+	@PostMapping("provider")
+	public Provider createProvider(
+			@RequestBody Provider provider,
+			HttpServletRequest request,
+			HttpServletResponse response
+		) {
+		providerSvc.createProvider(provider);
+			
+		if(provider == null) {
+			response.setStatus(400);
+		}
+		return provider;
+	}
+	
+	
+	
+
+}
+
+
+
