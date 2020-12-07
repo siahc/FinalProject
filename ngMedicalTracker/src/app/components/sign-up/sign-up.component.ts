f6cf6547f7a744223e06d2200b57279fda4089fe
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-12-07 14:28:19 -0600

    register form bootstrapped

--- ngMedicalTracker/src/app/components/sign-up/sign-up.component.ts
+++ ngMedicalTracker/src/app/components/sign-up/sign-up.component.ts
@@ -3,16 +3,14 @@ import { User } from 'src/app/models/user';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { error, log } from 'console';
 import { Patient } from 'src/app/models/patient';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
-
   newUser: User = new User();
   newPatient: Patient = new Patient();
 
@@ -20,45 +18,39 @@ export class SignUpComponent implements OnInit {
     private auth: AuthService,
     private router: Router,
     private patientService: PatientService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-  }
-  register(){
-  this.auth.register(this.newUser).subscribe(
-  data => {
-    console.log('register sign-up component method');
-    this.auth.login(this.newUser.username, this.newUser.password).subscribe(
-      data=> {
-        console.log('Sign-up successful');
-        this.registerInformation();
-      },
-          err => {
-            console.error(err)
+  register() {
+    this.auth.register(this.newUser).subscribe(
+      (data) => {
+        console.log('register sign-up component method');
+        this.auth.login(this.newUser.username, this.newUser.password).subscribe(
+          (data) => {
+            console.log('Sign-up successful');
+            this.registerInformation();
+          },
+          (err) => {
+            console.error(err);
           }
-    )
-  },
-   err => {
-    console.error(err)
-  });
-
-  }
-
-  registerInformation(){
-    this.patientService.createPatient(this.newPatient).subscribe(
-      data => {
-        console.log('create patient successful');
-        this.router.navigateByUrl('/howto')
+        );
       },
-      err => {
-        console.error(err)
+      (err) => {
+        console.error(err);
       }
-    )
+    );
   }
 
-
-
-
-
+  registerInformation() {
+    this.patientService.createPatient(this.newPatient).subscribe(
+      (data) => {
+        console.log('create patient successful');
+        this.router.navigateByUrl('/howto');
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }
 }
