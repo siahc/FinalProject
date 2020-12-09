fafb7475b9f360e58423ee5ad2e27997533cb371
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-12-08 23:46:57 -0600

    provider can see patient meds/history

--- ngMedicalTracker/src/app/services/provider.service.ts
+++ ngMedicalTracker/src/app/services/provider.service.ts
@@ -1,3 +1,4 @@
+import { MedicalHistory } from './../models/medical-history';
 import { catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -5,51 +6,68 @@ import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Provider } from '../models/provider';
 import { Patient } from '../models/patient';
+import { Medication } from '../models/medication';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProviderService {
-
   private baseUrl = 'http://localhost:8090/';
-  private url = this.baseUrl + 'api/provider';
+  private url = this.baseUrl + 'api/providerpatients';
 
-  constructor(
-    private http: HttpClient,
-    private auth: AuthService
-    ) { }
 
-    getHttpOptions() {
-      const credentials = this.auth.getCredentials();
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
-      const httpOptions = {
-        headers: new HttpHeaders({
-          Authorization: `Basic ${credentials}`,
-          'X-Requested-With': 'XMLHttpRequest'
-        })
-      };
-      return httpOptions;
-    }
-
-  showProvider(): Observable<Provider>{
+  getHttpOptions() {
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest',
+      }),
+    };
+    return httpOptions;
+  }
+  showProvider(): Observable<Provider> {
     const httpOptions = this.getHttpOptions();
     let providerURL = this.baseUrl + 'api/providerinfo';
     return this.http.get<Provider>(providerURL, httpOptions).pipe(
       catchError((err: any) => {
         console.log(err);
-        return throwError('providerService.showProvider(): Error showing provider');
-
+        return throwError(
+          'providerService.showProvider(): Error showing provider'
+        );
+      })
+    );
+  }
+  providerPatientList(): Observable<Patient[]> {
+    const httpOptions = this.getHttpOptions();
+    // let testUrl = this.baseUrl + 'api/providerpatients';
+    return this.http.get<Patient[]>(this.url, httpOptions).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError(
+          'PatientService.providerPatientInformation(): Failed to get patients'
+        );
+      })
+    );
+  }
+  providerViewPatientMeds(id:number): Observable<Medication[]>{
+    const httpOptions = this.getHttpOptions();
+    return this.http.get<Medication[]>(`${this.url}/${id}/medications`, httpOptions).pipe(
+      catchError((err:any)=>{
+        console.error(err);
+        return throwError('Provider.service.providerViewPatientMeds():Failed to retrieve med list');
       })
     )
-}
-providerPatientInformation():Observable<Patient[]>{
-  const httpOptions = this.getHttpOptions();
-  let testUrl = this.baseUrl + 'api/providerpatients';
-  return this.http.get<Patient[]>(testUrl, httpOptions).pipe(
-    catchError((err:any)=> {
-      console.error(err);
-      return throwError('PatientService.providerPatientInformation(): Failed to get patients')
-    })
-  )
-}
+  }
+  providerViewPatientHist(id:number): Observable<MedicalHistory[]>{
+    const httpOptions = this.getHttpOptions();
+    return this.http.get<MedicalHistory[]>(`${this.url}/${id}/history`, httpOptions).pipe(
+      catchError((err:any)=>{
+        console.error(err);
+        return throwError('Provider.service.providerViewPatientHist():Failed to retrieve med list');
+      })
+    )
+  }
 }
