36ed8695e95e548c844663862900db4ea29b75b7
Thomas Dang <Thomas.N.Dang@gmail.com>
2020-12-14 09:32:10 -0600

    Project ready for initial AWS deployment

--- MedicalTracker/src/main/java/com/skilldistillery/medicaltracker/MedicalTrackerApplication.java
+++ MedicalTracker/src/main/java/com/skilldistillery/medicaltracker/MedicalTrackerApplication.java
@@ -2,16 +2,23 @@ package com.skilldistillery.medicaltracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class MedicalTrackerApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(MedicalTrackerApplication.class, args);
-	}
+public class MedicalTrackerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MedicalTrackerApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalTrackerApplication.class, args);
+    }
 	@Bean
 	public PasswordEncoder configurePasswordEncoder() {
 		return new BCryptPasswordEncoder();
